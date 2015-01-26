fa64e8b0a025b61c714bd53bda1bf1662e259ffd
Krzysztof Burlinski <krzysztof.burlinski@gmail.com>
2015-01-26 23:29:52 +0100

    Small gitignore impreovement.

--- test/app.js
+++ test/app.js
@@ -2,5 +2,5 @@ function start() {
 	var canvas = document.getElementById("map-canvas");
 	var map = new WesnothTiles.Map(canvas);
     map.Resize(window.innerWidth, window.innerHeight);
-	map.Redraw();
+    map.Redraw();
 }
